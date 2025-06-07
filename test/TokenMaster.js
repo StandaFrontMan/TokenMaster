cf42a13f3982024fcb4b2e6b92d79338ba5f8b4c
Stanislav Vin <vinninjaq@gmail.com>
2025-06-08 00:39:39 +0200

    feature: declare the owner val in contarct, test

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,24 +1,36 @@
 const { expect } = require("chai");
 
+const NAME = "TokenMaster";
+const SYMBOL = "TM";
+
 describe("TokenMaster", () => {
   let tokenMaster;
+  let deployer, buyer;
 
   beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+
     const TokenMaster = await ethers.getContractFactory("TokenMaster");
-    tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
+    tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
   });
 
   describe("Deployment", () => {
     it("Sets the name", async () => {
       let name = await tokenMaster.name();
 
-      expect(name).to.equal("TokenMaster");
+      expect(name).to.equal(NAME);
     });
 
     it("Sets the symbol", async () => {
       let symbol = await tokenMaster.symbol();
 
-      expect(symbol).to.equal("TM");
+      expect(symbol).to.equal(SYMBOL);
+    });
+
+    it("Sets the owner", async () => {
+      const owner = await tokenMaster.owner();
+
+      expect(owner).to.equal(deployer.address);
     });
   });
 });
